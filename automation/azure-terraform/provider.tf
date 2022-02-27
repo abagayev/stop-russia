536ab2686646fe7d9506971f592c9e09d2a9551c
crypton <crypton@crypton-technologies.net>
2022-02-26 19:11:19 -0800

    Use Azure container instances instead + create resource group if not exists

--- automation/azure-terraform/provider.tf
+++ automation/azure-terraform/provider.tf
@@ -1,6 +1,5 @@
 provider "azurerm" {
     features {}
-    version = "=2.50.0"
 }
 
 data "azurerm_client_config" "current" {}
--- automation/azure-terraform/variables.tf
+++ automation/azure-terraform/variables.tf
@@ -2,6 +2,18 @@ variable "resource_group_name" {
   default = "stoprussia"
 }
 
-variable "name" {
-  default = "stoprussia-CHANGEME"
+variable "region" {
+  default = "West Europe"
+}
+
+variable "cpu_spec" {
+  default = "0.5"
+}
+
+variable "mem_spec" {
+  default = "0.5"
+}
+
+output "name" {
+  value = azurerm_container_group.stoprussia.name
 }
