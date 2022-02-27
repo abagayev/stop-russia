917f70b29caec5938e6d7eaabd429120446257d0
crypton <crypton@crypton-technologies.net>
2022-02-26 19:29:16 -0800

    sha1 only the tenant_id so that subsequent `terraform apply` calls aren't recreating resource

--- automation/azure-terraform/container_instance.tf
+++ automation/azure-terraform/container_instance.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "stoprussia" {
 }
 
 resource "azurerm_container_group" "stoprussia" {
-    name                = sha1("stoprussia--${timestamp()}")
+    name                = sha1("stoprussia-${data.azurerm_client_config.current.tenant_id}") # sha1 for unique name that is automagic
     location            = azurerm_resource_group.stoprussia.location
     resource_group_name = azurerm_resource_group.stoprussia.name
 
