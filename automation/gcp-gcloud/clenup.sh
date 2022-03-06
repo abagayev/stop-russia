b9da1fed53f1a40e7e0bc9506b3ad2a25a5ad2f2
Artem Vasylkin <artemvasilkin14@gmail.com>
2022-03-06 20:23:07 +0200

    [scripts] fix clenup script to correct spelling (backward-compatible)

--- automation/gcp-gcloud/clenup.sh
+++ automation/gcp-gcloud/clenup.sh
@@ -1,34 +1,6 @@
 #!/usr/bin/env bash
 
-shopt -s globstar
+# `clenup.sh` існує для зворотньої сумісності
+# `clenup.sh` exists for backward compatibility
 
-# Ensure that the CWD is set to script's location
-cd "${0%/*}"
-CWD=$(pwd)
-
-#######
-
-TAG="stop-ru"
-
-VMs=()
-
-echo "May take a while..."
-
-for vm in $(gcloud compute instances list --format="value(name)")
-do
-	if [[ $vm = $TAG-* ]]
-	then
-		echo "Will delete $vm"
-		VMs+=($vm)
-	fi
-done
-
-# if there are Vms to delete
-if (( ${#VMs[@]} ))
-then
-	gcloud compute instances delete --zone=asia-northeast3-a -q ${VMs[@]}
-else
-	echo "No VMs to delete"
-fi
-
-echo "Done!"
+./cleanup.sh
